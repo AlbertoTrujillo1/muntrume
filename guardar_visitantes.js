c376cce2c32697584bb5392e9935b2d17278e95a
AlbertoTrujillo1 <albertito.magic14@gmail.com>
2024-10-26 19:01:40 +0200

    Update guardar_visitantes.js

--- guardar_visitantes.js
+++ guardar_visitantes.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", async () => {
   try {
+    // Obtener la IP del visitante
     const ipResponse = await fetch('https://api.ipify.org?format=json');
     const ipData = await ipResponse.json();
     const ip = ipData.ip;
 
+    // Obtener el userAgent del navegador
     const navegador = navigator.userAgent;
 
+    // Enviar los datos al Google Apps Script
     const response = await fetch("https://script.google.com/macros/s/AKfycbwdIYLnfIpGqGrJtwWDHSEA584Y4KtxJHytgwBHVYtXKy3AKOS9i7Ax9EkZBeQH5qpTMQ/exec", {
       method: "POST",
       headers: {
@@ -14,6 +17,7 @@ document.addEventListener("DOMContentLoaded", async () => {
       body: `ip=${encodeURIComponent(ip)}&navegador=${encodeURIComponent(navegador)}`
     });
 
+    // Obtener la respuesta del servidor
     const result = await response.text();
     console.log('Resultado del envío:', result);
   } catch (error) {
